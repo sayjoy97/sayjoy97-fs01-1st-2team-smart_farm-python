922e2213f0a4aa35d40dda3bb19ff4cb7018613b
sayjoy <sayjjoy97@gmail.com>
2025-10-25 22:32:00 +0900

    dht11.py 추가

---
+++ smartfarm/sensor/dht11.py
@@ -0,0 +1,24 @@
+# 온/습도 센서
+import time
+import board
+import adafruit_dht
+
+class DHT11:
+    def __init__(self, pin):
+        self.dht_device = adafruit_dht.DHT11(pin)
+
+
+    def read(self):
+        while True:
+            try:
+                measured_temp = self.dht_device.temperature
+                measured_humidity = self.dht_device.humidity
+                time.sleep(900)
+            except RuntimeError as e:
+                print(f"Error reading DHT11 sensor: {e}")
+            finally:
+                return measured_temp, measured_humidity
+
+
+    def close(self):
+        self.dht_device.exit()
